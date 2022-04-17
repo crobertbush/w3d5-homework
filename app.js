bf3ef2dbf4f6289cd2e9128321b84d9aac6ff345
Robby Bush <crobertbush@gmail.com>
2022-04-17 12:25:33 -0400

    update

---
+++ app.js
@@ -0,0 +1,47 @@
+//Variables
+var a = 15;
+var b = 20;
+
+//addition
+function add(param1, param2){
+    console.log(param1 + param2);
+}
+add(a,b);
+
+add = (param1, param2) => {
+    console.log(param1 + param2);
+}
+add(a,b);
+
+//subtraction
+function subtract(param1, param2){
+    console.log(param1 - param2);
+}
+subtract(a,b);
+
+subtract = (param1, param2) => {
+    console.log(param1 - param2);
+}
+subtract(a,b);
+
+//multiplication
+function multiply(param1, param2){
+    console.log(param1 * param2);
+}
+multiply(a,b);
+
+multiply = (param1, param2) => {
+    console.log(param1 * param2);
+}
+multiply(a,b);
+
+//division
+function divide(param1, param2){
+    console.log(param1 / param2);
+}
+divide(a,b);
+
+divide = (param1, param2) => {
+    console.log(param1 / param2);
+}
+divide(a,b);
